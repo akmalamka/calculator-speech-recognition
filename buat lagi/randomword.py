8c666a57b06f26d6ccc83f7e5a9d8d251b3749d8
Muhammad Akmal <akmalm@Muhammads-MacBook-Pro.local>
2020-12-11 09:49:52 +0700

    update readme

--- buat lagi/randomword.py
+++ buat lagi/randomword.py
@@ -16,17 +16,3 @@ for i in range(100):
     f.write("%s\r\n" % (arr[i]))
     
 f.close()
-# r = RandomWords()
-# print("Hello world")
-# Return a single random word
-# print(' '.join(r.get_random_words(minLength=5, maxLength=10)))
-# abc = r.get_random_words(minLength=5, maxLength=10)
-# print(abc)
-# # Return list of Random words
-# for i in range(100):
-# print(' '.join(r.get_random_words(minLength=5, maxLength=10, limit=11)))
-# a = r.get_random_words(minLength=5, maxLength=10, limit=11)
-# b = ' '.join(a)
-# print(b)
-# # Return Word of the day
-# print(r.word_of_the_day())
